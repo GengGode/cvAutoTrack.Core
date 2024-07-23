876cb2ea90ee46e8051a604a71c3a190fda3372c
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-07-23 18:15:05 +0800

    refactor: 修改窗口图形工具函数以使用winrt::check_hresult

--- source/frame/capture/utils/utils.window_graphics.h
+++ source/frame/capture/utils/utils.window_graphics.h
@@ -61,7 +61,7 @@ namespace tianli::frame::capture::utils::window_graphics
         auto activation_factory = winrt::get_activation_factory<winrt::Windows::Graphics::Capture::GraphicsCaptureItem>();
         auto interop_factory = activation_factory.as<IGraphicsCaptureItemInterop>();
         winrt::Windows::Graphics::Capture::GraphicsCaptureItem item = { nullptr };
-        interop_factory->CreateForWindow(hwnd, winrt::guid_of<ABI::Windows::Graphics::Capture::IGraphicsCaptureItem>(), reinterpret_cast<void**>(winrt::put_abi(item)));
+        winrt::check_hresult(interop_factory->CreateForWindow(hwnd, winrt::guid_of<ABI::Windows::Graphics::Capture::IGraphicsCaptureItem>(), winrt::put_abi(item)));
         return item;
     }
 
