3fc46faf603d766419e72f3a14040518b5fcec96
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-08-13 18:47:01 +0800

    refactor: 使用 GL_BGRA_EXT 替换 GL_RGBA 以修复图像显示异常

--- source/gui/debugger.cpp
+++ source/gui/debugger.cpp
@@ -18,7 +18,7 @@ static auto bind_texture=[](cv::Mat& mat, ImTextureID& texture_id, bool need_to_
     glBindTexture(GL_TEXTURE_2D, texture_cast(texture_id));
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mat.cols, mat.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, mat.data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA_EXT, mat.cols, mat.rows, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, mat.data);
 };
 static auto rebind_texture=[](cv::Mat& mat, ImTextureID& texture_id, bool need_to_rgba)
 {
@@ -27,7 +27,7 @@ static auto rebind_texture=[](cv::Mat& mat, ImTextureID& texture_id, bool need_t
     if (need_to_rgba)
         cv::cvtColor(mat, mat, cv::COLOR_BGRA2RGBA);
     glBindTexture(GL_TEXTURE_2D, texture_cast(texture_id));
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mat.cols, mat.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, mat.data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA_EXT, mat.cols, mat.rows, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, mat.data);
 };
     
 
