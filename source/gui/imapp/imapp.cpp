915eed930a22106c0e52f676d2c6e5840e7bfbc1
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-08-13 22:21:40 +0800

    feat: 添加小型转储文件生成功能以捕获异常信息
    引入了 `windows.h`、`DbgHelp.h` 和 `tchar.h` 头文件。
    添加了内联函数 `GenerateMiniDump`，用于生成小型转储文件。
    添加了内联函数 `ExceptionFilter`，用于处理未捕获的异常。
    在 `imapp::execute` 函数中，设置了未处理异常过滤器 `SetUnhandledExceptionFilter`。
    修改了 `imapp::execute` 函数的返回逻辑，增加了 `try-catch` 块。

--- source/gui/imapp/imapp.cpp
+++ source/gui/imapp/imapp.cpp
@@ -185,10 +185,66 @@ bool imapp::uninitialized()
     return true;
 }
 
+
+#include <windows.h>
+#include "DbgHelp.h"
+#include "tchar.h"
+inline int GenerateMiniDump(PEXCEPTION_POINTERS pExceptionPointers)
+{
+    typedef BOOL(WINAPI* MiniDumpWriteDumpT)(HANDLE, DWORD, HANDLE, MINIDUMP_TYPE, PMINIDUMP_EXCEPTION_INFORMATION, PMINIDUMP_USER_STREAM_INFORMATION, PMINIDUMP_CALLBACK_INFORMATION);
+    MiniDumpWriteDumpT pfnMiniDumpWriteDump = NULL;
+    HMODULE hDbgHelp = LoadLibrary(_T("DbgHelp.dll"));
+    if (NULL == hDbgHelp)
+    {
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+    pfnMiniDumpWriteDump = (MiniDumpWriteDumpT)GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
+
+    if (NULL == pfnMiniDumpWriteDump)
+    {
+        FreeLibrary(hDbgHelp);
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+    TCHAR szFileName[MAX_PATH] = { 0 };
+    TCHAR szVersion[] = TEXT("imapp");
+    SYSTEMTIME stLocalTime;
+    GetLocalTime(&stLocalTime);
+    wsprintfA(szFileName, "%s-%04d%02d%02d-%02d%02d%02d.dmp", szVersion, stLocalTime.wYear, stLocalTime.wMonth, stLocalTime.wDay, stLocalTime.wHour, stLocalTime.wMinute, stLocalTime.wSecond);
+    HANDLE hDumpFile = CreateFile(szFileName, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+    if (INVALID_HANDLE_VALUE == hDumpFile)
+    {
+        FreeLibrary(hDbgHelp);
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+    MINIDUMP_EXCEPTION_INFORMATION expParam;
+    expParam.ThreadId = GetCurrentThreadId();
+    expParam.ExceptionPointers = pExceptionPointers;
+    expParam.ClientPointers = FALSE;
+    pfnMiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hDumpFile, MiniDumpWithDataSegs, (pExceptionPointers ? &expParam : NULL), NULL, NULL);
+    CloseHandle(hDumpFile);
+    FreeLibrary(hDbgHelp);
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+inline LONG WINAPI ExceptionFilter(LPEXCEPTION_POINTERS lpExceptionInfo)
+{
+    if (IsDebuggerPresent())
+    {
+        return EXCEPTION_CONTINUE_SEARCH;
+    }
+    return GenerateMiniDump(lpExceptionInfo);
+}
+
 bool imapp::execute()
 {
+    SetUnhandledExceptionFilter(ExceptionFilter);
     if (!this->initialization())
         return false;
     this->frame_loop();
-    return this->uninitialized();
+    try {
+        return this->uninitialized();
+    }
+    catch (...)
+    {
+        return false;
+    }
 }
